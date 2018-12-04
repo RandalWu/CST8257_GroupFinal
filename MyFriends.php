5e653a7e9a0c591b1118dc010af81bab04077a30
Kyle Leslie <31903847+kyleslie2@users.noreply.github.com>
2018-12-04 14:07:20 -0500

    stated tables for MyFriends

--- MyFriends.php
+++ MyFriends.php
@@ -13,6 +13,8 @@ if (!isset($_SESSION['loggedInUser'])) {
     $_SESSION["fromPage"]= "MyFriends";
     header('Location: index.php');
 }
+$myName = $_SESSION['loggedInUser']->getName();
+$myID = $_SESSION['loggedInUser']->getID();
 
 
 ?>
@@ -21,6 +23,56 @@ if (!isset($_SESSION['loggedInUser'])) {
 
         <h1 align="center">My Friends!</h1>
         <hr>
+    <div  align="left">
+        <h4 align="left">Welcome <b><?php echo $myName;?></b>! (not you? You can change users <a href="Login.php">here</a>)</h4>
+        <br>
+        <p><b>Friends</b> <a style="float:right;" href="AddFriend.php">Add Friends</a> </p>
+        <hr>
+
+    </div>
+
+        <table class="table table-striped">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Shared Albums</th>
+                <th scope="col">Defriend</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <?php
+            //for each "friend" of myID, print name + album
+            //create class for friend + shared album info?
+
+            ?>
+            <tr>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+            </tbody>
+        </table>
+
+        <div align="right">
+            <input class="btn btn-danger" type="submit" name="btnDefriend" value="Defriend"
+                   onclick="return confirm('The selected friends will be defriended!')"/>
+
+        </div>
+
+
+
+
 
 
     </div>
